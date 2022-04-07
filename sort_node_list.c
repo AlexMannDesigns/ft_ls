f1ac761b3d241ee6301ded88b1de2e3555b174b2
Alexander Mann <amann@c1r4p7.hive.fi>
2022-04-07 16:32:40 +0300

    added selection sort algo to optimise program

--- sort_node_list.c
+++ sort_node_list.c
@@ -6,56 +6,91 @@
 /*   By: amann <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 18:02:23 by amann             #+#    #+#             */
-/*   Updated: 2022/03/31 15:15:10 by amann            ###   ########.fr       */
+/*   Updated: 2022/04/07 16:31:02 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_ls.h"
 
-static void	node_list_swap(t_list **list, t_list **head)
+void static	update_original(t_list **lst, t_list *min)
 {
-	t_list	*b;
-	t_list	*c;
-	t_list	*temp;
+	t_list	*head;
 
-	b = (*list)->next;
-	c = (*list)->next->next;
-	(*list)->next->next = *list;
-	(*list)->next = c;
-	if (*list == *head)
+	head = *lst;
+	if (*lst == min)
 	{
-		*head = b;
+		*lst = (*lst)->next;
 		return ;
 	}
-	temp = *head;
-	while ((*head)->next != *list)
-		*head = (*head)->next;
-	(*head)->next = b;
-	*head = temp;
-	*list = *head;
+	while ((*lst)->next != min)
+		*lst = (*lst)->next;
+	(*lst)->next = (*lst)->next->next;
+	*lst = head;
 }
 
-void	sort_node_list(t_list **list, t_ls *flags)
+void static	update_sorted(t_list **sorted, t_list *min)
 {
-	t_list		*head;
-	t_file_info	*current;
-	t_file_info	*next;
+	t_list	*head;
 
-	head = *list;
-	while ((*list)->next)
+	head = *sorted;
+	if (!(*sorted))
+		*sorted = min;
+	else
 	{
-		current = (t_file_info *)(*list)->content;
-		next = (t_file_info *)(*list)->next->content;
-		if (standard_lexico(current, next, flags))
-			node_list_swap(list, &head);
-		else if (reverse_lexico(current, next, flags))
-			node_list_swap(list, &head);
-		else if (standard_time(current, next, flags))
-			node_list_swap(list, &head);
-		else if (reverse_time(current, next, flags))
-			node_list_swap(list, &head);
-		else
-			*list = (*list)->next;
+		while ((*sorted)->next)
+			*sorted = (*sorted)->next;
+		(*sorted)->next = min;
+		*sorted = head;
 	}
-	*list = head;
+}
+
+static t_list	*selection_sort_control(t_list **lst, t_ls *flags)
+{
+	t_list		*min;
+	t_file_info	*current;
+	t_file_info	*min_info;
+
+	min = NULL;
+	while (*lst)
+	{
+		current = (t_file_info *)(*lst)->content;
+		if (min)
+		{
+			min_info = (t_file_info *) min->content;
+			if (standard_lexico(min_info, current, flags))
+				min = *lst;
+			else if (reverse_lexico(min_info, current, flags))
+				min = *lst;
+			else if (standard_time(min_info, current, flags))
+				min = *lst;
+			else if (reverse_time(min_info, current, flags))
+				min = *lst;
+		}
+		else
+			min = *lst;
+		*lst = (*lst)->next;
+	}
+	return (min);
+}
+
+void	sort_node_list(t_list **lst, t_ls *flags)
+{
+	t_list	*sorted;
+	t_list	*head;
+	t_list	*min;
+
+	sorted = NULL;
+	min = NULL;
+	head = *lst;
+	while (*lst)
+	{
+		min = selection_sort_control(lst, flags);
+		*lst = head;
+		update_sorted(&sorted, min);
+		update_original(lst, min);
+		head = *lst;
+		min->next = NULL;
+		min = NULL;
+	}
+	*lst = sorted;
 }
